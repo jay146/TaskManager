2c7088bb40b241b0f4cbb5163df1d607c44f52b2
jay146 <jayakrishna.jk14@gmail.com>
2021-09-28 20:49:45 +0530

    used propeerty binding in accordion box3

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,3 +1,4 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  // property and datatype
   Designation: string = "";
   Username: string = "";
   NoOfTeamMembers: number = 0;
@@ -20,7 +22,9 @@ export class DashboardComponent implements OnInit {
   Projects: string[] = [];
   Years: number[] = [];
   TeamMembersSummary: any = [];
-  TeamMembers: any = [];
+  Accdata:any=[];
+
+
 
   constructor() { }
 
@@ -34,6 +38,45 @@ export class DashboardComponent implements OnInit {
   this.ProjectCost = 2113507;
   this.CurrentExpenditure = 96788;
   this.AvailableFunds = 52536;
- }
+  this.Clients=["hcl","google","wipro"];
+  this.Projects=["Project-A","Project-B","Project-C","Project-D","Project-Z"];
+  for(var i=2019;i>=2010;i--){
+    if(i==2017){
+     break;
+    }
+    this.Years.push(i);
+  }
 
+this.TeamMembersSummary=[ {Region:"East",TeamMembersCount:10,TemporaryUnavailableMembers:5},
+                          {Region:"West",TeamMembersCount:20,TemporaryUnavailableMembers:6},
+                          {Region:"North",TeamMembersCount:30,TemporaryUnavailableMembers:4},
+                          {Region:"South",TeamMembersCount:40,TemporaryUnavailableMembers:6}
+                        ]
+
+this.Accdata=[
+  {region:"East", members:
+    [{Id:1,Name:"ford",Status:"available"},
+     {Id:2,Name:"bmw",Status:"busy"}
+    ]
+  },
+
+  {region:"NORTH", members:
+   [ {Id:1,Name:"ford",Status:"available"},
+     {Id:2,Name:"bmw",Status:"busy"}
+   ]
+  },
+  {region:"SOUTH", members:
+   [{Id:1,Name:"ford",Status:"available"},
+    {Id:2,Name:"bmw",Status:"busy"}
+   ]
+},
+  {region:"Wast", members:
+   [{Id:1,Name:"ford",Status:"available"},
+    {Id:2,Name:"bmw",Status:"busy"}
+   ]
 }
+];
+
+
+          }
+        }
